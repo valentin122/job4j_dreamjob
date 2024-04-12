f0ee1f47a7b020173916a6c17a31fb23bb880741
Valentin <val1915@yandex.ru>
2024-04-12 19:37:49 +0300

    Список кандидатов

---
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -0,0 +1,19 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
+
+@Controller
+@RequestMapping("/candidates")
+public class CandidateController {
+    private final MemoryCandidateRepository candidateRepository = MemoryCandidateRepository.getInstance();
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("candidates", candidateRepository.findAll());
+        return "candidates/list";
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,62 @@
+package ru.job4j.dreamjob.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.Vacancy;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class MemoryCandidateRepository implements CandidateRepository {
+
+    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    private MemoryCandidateRepository() {
+        save(new Candidate(0, "Intern Java Developer", "Intern description", LocalDateTime.now()));
+        save(new Candidate(0, "Junior Java Developer", "Junior description", LocalDateTime.now()));
+        save(new Candidate(0, "Junior+ Java Developer", "Junior+ description", LocalDateTime.now()));
+        save(new Candidate(0, "Middle Java Developer", "Middle description", LocalDateTime.now()));
+        save(new Candidate(0, "Middle+ Java Developer", "Middle+ description", LocalDateTime.now()));
+        save(new Candidate(0, "Senior Java Developer", "Senior description", LocalDateTime.now()));
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        candidates.remove(id);
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(),
+                (id, oldCandidate) -> new Candidate(oldCandidate.getId(), candidate.getName())) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return Optional.ofNullable(candidates.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}

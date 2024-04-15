ec09e8a1a8b36dc61e76fbe74916ae77e1853dea
Valentin <val1915@yandex.ru>
2024-04-15 23:14:41 +0300

    checkStyle

--- src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 @Service
 public class SimpleCandidateService implements CandidateService {
 
-
     private final CandidateRepository candidateRepository;
 
     private SimpleCandidateService(CandidateRepository candidateRepository) {
